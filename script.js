c21e9443634eaa5545969ad9f18a235a135a5da3
Rachel Colby <rachelcolby11@gmail.com>
2016-04-05 00:51:09 -0700

    Refactor jQuery script

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var videoLinks = $('a');
 
 for (var i = 0; i < videoLinks.length; i++) {
-  // Generate a thumbnail URL
-  var thumb = youtube.generateThumbnailUrl($(videoLinks[i]).attr('href'));
-  // Create an IMG element, append the IMG to the link, and set its "src" 
+  // Create an IMG element, append the IMG to the link, and set its "src" to create a thumbnail
   $(videoLinks[i]).append(
-  $('<img/>')
-    .attr('src', youtube.generateThumbnailUrl($(videoLinks[i]).attr('href')))
+    $('<img/>').attr('src', youtube.generateThumbnailUrl(
+      $(videoLinks[i]).attr('href'))
+    )
   );
 }
