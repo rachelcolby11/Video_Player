64039778e984fc224013f74e804ca42dffcea1ee
Rachel Colby <rachelcolby11@gmail.com>
2016-04-04 23:59:14 -0700

    Change script to jquery

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-var videoLinks = document.getElementsByTagName('a');
+var videoLinks = $('a');
 
 for (var i = 0; i < videoLinks.length; i++) {
-  // Store the current href into a variable
-  var videoUrl = videoLinks[i].getAttribute('href');
   // Generate a thumbnail URL
-  var thumb = youtube.generateThumbnailUrl(videoUrl);
-  // Create an IMG element and set its "src"
-  var img = document.createElement('img');
-  img.setAttribute('src', thumb);
-  // Append the IMG to the link
-  videoLinks[i].appendChild(img);
+  var thumb = youtube.generateThumbnailUrl($(videoLinks[i]).attr('href'));
+  // Create an IMG element, append the IMG to the link, and set its "src" 
+  $(videoLinks[i]).append(
+  $('<img/>')
+    .attr('src', youtube.generateThumbnailUrl($(videoLinks[i]).attr('href')))
+  );
 }
