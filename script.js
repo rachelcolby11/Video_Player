d378f2bb2d92dbf0ad368893e2603fadf7c5e158
Rachel Colby <rachelcolby11@gmail.com>
2016-04-11 16:01:42 -0700

    Refactor jQuery using addThumbnail function

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var videoLinks = $('a');
 
-for (var i = 0; i < videoLinks.length; i++) {
-  // Create an IMG element, append the IMG to the link, and set its "src" to create a thumbnail
-  $(videoLinks[i]).append(
+function addThumbnail (link) {
+// Create an IMG element, append the IMG to the given link, and set its "src" to create a thumbnail
+  $(link).append(
     $('<img/>').attr('src', youtube.generateThumbnailUrl(
-      $(videoLinks[i]).attr('href'))
+      $(link).attr('href'))
     )
   );
+};
+
+for (var i = 0; i < videoLinks.length; i++) {
+  addThumbnail(videoLinks[i]);
 }
